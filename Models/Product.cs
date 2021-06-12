1b5ca785cd8e5727e8043b6377f81d75c9d425ca
Mateusz Basiak <mateusz.basiak@gmail.com>
2021-06-12 15:13:23 +0200

    class Products created

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ContosoCrafts.website.Models
@@ -8,12 +10,17 @@ namespace ContosoCrafts.website.Models
     public class Product
     {
         public string Id { get; set; }
+       
         public string Maker { get; set; }
+        [JsonPropertyName("img")]
         public string Image { get; set; }
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
         
+
     }
 }
